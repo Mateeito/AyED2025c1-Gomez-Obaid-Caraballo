939433ae036f06b04255b28f2cd289ff0cde14f4
Your Name <you@example.com>
2025-03-26 12:41:53 -0300

    agregue las estructuras para las funciones de la actividad 2 del tp1

--- TrabajoPractico_1/actividad 2/modules/modulo1.py
+++ TrabajoPractico_1/actividad 2/modules/modulo1.py
@@ -1,2 +1,37 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+
+
+
+
+
+
+def esta_vacia():
+    pass
+
+def __len__():
+    pass
+
+def agregar_al_inicio(item):
+    pass
+
+def agregar_al_final(item):
+    pass
+
+def insertar(item, posicion):
+    pass
+
+def extraer(posicion):
+    pass
+
+def copiar():
+    pass
+
+def invertir():
+    pass
+
+def concatenar(lista):
+    pass
+
+def __add__(lista):
+    pass
