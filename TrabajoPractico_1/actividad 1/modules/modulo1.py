d0a7ad84c1517db482422aab98d576c059dff714
Your Name <you@example.com>
2025-03-26 12:31:07 -0300

    agregue la estructura de las funciones de los diferentes metodos de ordenamiento

--- TrabajoPractico_1/actividad 1/modules/modulo1.py
+++ TrabajoPractico_1/actividad 1/modules/modulo1.py
@@ -1,2 +1,12 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+
+
+def ordenamiento_burbuja():
+    pass
+
+def ordenamiento_quicksort():
+    pass
+
+def ordenamiento_por_residuos():
+    pass
--- TrabajoPractico_1/actividad 1/tests/test_modulo1.py
+++ TrabajoPractico_1/actividad 1/tests/test_modulo1.py
@@ -1,1 +1,23 @@
 # Archivo de test para realizar pruebas unitarias del modulo1
+
+#Colocar aca las pruebas para el ordenamiento burbuja
+
+
+
+def prueba_numeros_altos_burbuja():
+    #
+    pass
+
+
+
+# Colocar aca las pruebas para el ordenamiento Quicksort
+
+
+
+
+#Colocar aca las pruebas para el ordenamiento por Residuos
+
+
+
+
+
